309916e826c0f002f1304e8eda6410f4ad576612
kkiwt20006 <kiwtnoob0@gmail.com>
2025-09-26 23:05:00 +0700

    Update cái khung

---
+++ Cinema_Management/Cinema_Management/PhanDangKy.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Cinema_Management
+{
+    partial class PhanDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NutDangKy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // NutDangKy
+            // 
+            this.NutDangKy.BackColor = System.Drawing.Color.Transparent;
+            this.NutDangKy.Font = new System.Drawing.Font("Noto Serif SC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NutDangKy.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.NutDangKy.Location = new System.Drawing.Point(508, 603);
+            this.NutDangKy.Name = "NutDangKy";
+            this.NutDangKy.Size = new System.Drawing.Size(261, 68);
+            this.NutDangKy.TabIndex = 0;
+            this.NutDangKy.Text = "Đăng Ký";
+            this.NutDangKy.UseVisualStyleBackColor = false;
+            this.NutDangKy.Click += new System.EventHandler(this.NutDangKy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(463, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đăng Ký Tài Khoản";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(845, 448);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(90, 20);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Đăng Nhập";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1294, 721);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NutDangKy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NutDangKy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Cinema_Management/Cinema_Management/PhanDangKy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema_Management
+{
+    public partial class PhanDangKy : Form
+    {
+        public PhanDangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void NutDangKy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            PhanDangNhap DangNhap = new PhanDangNhap();
+            DangNhap.ShowDialog();
+        }
+    }
+}
