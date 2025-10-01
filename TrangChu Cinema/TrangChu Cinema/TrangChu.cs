2003f7678568b5bf3369be09e3ce02b61cb2f4c9
Thai Hung <nguyenthaaihung111026@gmail.com>
2025-10-01 12:21:24 +0700

    Thêm giao diện sau khi đăng kí hoặc đăng nhập thành công

---
+++ TrangChu Cinema/TrangChu Cinema/TrangChu.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrangChu_Cinema
+{
+    public partial class TrangChu : Form
+    {
+        public TrangChu()
+        {
+            InitializeComponent();
+        }
+
+        private void phimToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrangChu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tàiKhoảnCủaTôiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuĐặtVé_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thànhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuĐangChiếu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuSắpChiếu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Đangchieu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PhimDangChieu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
